cbf5a39afc2614ce7f247a3388e6d40d8525265d
Jatin Gidh <134076892+Jatin0410@users.noreply.github.com>
2026-01-09 18:18:26 +0530

    Commit

---
+++ jan-09.cs
@@ -0,0 +1,3 @@
+<p a><p b><style>*{background:#ECE3DF;margin:0;position:absolute;}[a]{height:125;width:260;background:#2B2A4A;left:70;box-shadow:0px 175px#2B2A4A;}[b]{height:25.5;width:140;background:#2B2A4A;margin:99.5 0 0 290;box-shadow:-300px 75px #2B2A4A;}
+  
+  
